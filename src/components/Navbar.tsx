d0975cc6cc212c76988c3db47baa2351074899bb
Sawera Batool <sawerakiyani21@gmail.com>
2024-12-28 19:06:08 +0500

    Update Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,20 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { AiOutlineMenu } from "react-icons/ai";
 
 const Navbar = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
   return (
-    <div className="w-full pt-8 px-4"> 
+    <div className="w-full pt-8 px-4">
       <div className="flex justify-between items-center">
         <div className="text-xl font-medium">Sawera</div>
-        <ul className='gap-10 lg:gap-16 hidden md:flex'> 
-  <li className='menuLink'><a href='#hero'>Home</a></li>
-  <li className='menuLink'><a href='#about'>About</a></li>
-  <li className='menuLink'><a href='#projects'>Projects</a></li>
-  <li className='menuLink'><a href='#skills'>Skills</a></li> 
-  <li className='menuLink'><a href='#contact'>Contact</a></li> 
-</ul>
-
-        <AiOutlineMenu className="md:hidden" size={30} />
+        <ul className={`gap-10 lg:gap-16 ${isMenuOpen ? 'flex' : 'hidden'} md:flex`}>
+          <li className="menuLink cursor-pointer"><a href="#hero">Home</a></li>
+          <li className="menuLink cursor-pointer"><a href="#about">About</a></li>
+          <li className="menuLink cursor-pointer"><a href="#projects">Projects</a></li>
+          <li className="menuLink cursor-pointer"><a href="#skills">Skills</a></li>
+          <li className="menuLink cursor-pointer"><a href="#contact">Contact</a></li>
+        </ul>
+        <AiOutlineMenu
+          className="md:hidden"
+          size={30}
+          onClick={() => setIsMenuOpen(!isMenuOpen)}  
+        />
       </div>
     </div>
   );
