884d5159b4049f629c17d4f14531c6df086a88d4
Sawera Batool <sawerakiyani21@gmail.com>
2024-12-28 19:08:03 +0500

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,8 +6,23 @@ import Hero from "@/components/Hero";
 import Projects from "@/components/Projects";
 import Skills from "@/components/skills"; 
 import Footer from "@/components/Footer"; 
+import AOS from "aos"
+import "aos/dist/aos.css"
+import { useEffect } from "react";
 
 export default function Home() {
+  useEffect(() => {
+    AOS.init({
+      easing: "ease-out-back",
+      duration: 1200,
+      delay:100,
+      mirror: true,
+      anchorPlacement: "bottom-bottom",
+      offset: 160,
+    });
+    AOS.refresh()
+  }, [])
+  
   return (
     <main id="home">
       <Hero />
