f07586145934e397728a8ccdd7087a9af0c541ad
Sawera Batool <sawerakiyani21@gmail.com>
2024-12-28 19:02:57 +0500

    Update About.tsx

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -2,11 +2,14 @@ import React from 'react';
 
 const About = () => {
   return (
-    <div id='about' className='container mx-auto pt-32 px-4'>
-      <h2 className='text-3xl md:text-4xl font-bold text-center mb-6'>
+    <div id="about" className="container mx-auto pt-16 sm:pt-24 md:pt-32 px-4">
+      <h2 className="text-2xl sm:text-3xl md:text-4xl font-bold text-center mb-6">
         Get To Know Me
       </h2>
-      <p className='text-[#283432E6] text-lg md:text-xl leading-relaxed text-center md:text-left' data-aos="flip-down">
+      <p
+        className="text-[#283432E6] text-base sm:text-lg md:text-xl leading-relaxed text-center md:text-left"
+        data-aos="flip-down"
+      >
         I am a student at GIAIC, pursuing a comprehensive course in Artificial Intelligence, Web 3.0, and the Metaverse.
         Passionate about technology, I am dedicated to building a strong foundation in these advanced fields and 
         continuously learning new skills to stay up-to-date with the latest innovations. I am eager to explore 
